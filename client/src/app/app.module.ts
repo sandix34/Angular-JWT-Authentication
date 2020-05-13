6fb1028453398a8abe491e38b937247c75114272
sandrine <sandrinezocca@gmail.com>
2020-05-13 16:36:18 +0200

    install basic application

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LayoutModule } from './shared/layout/layout.module';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { TopbarComponent } from './shared/components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SignupComponent,
+    SigninComponent,
+    TopbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
