9b3f0494b6c43ee298f4e37a874b745fbb60977a
sandrine <sandrinezocca@gmail.com>
2020-05-18 17:15:44 +0200

    authentication service and sigin method

--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -1,18 +1,52 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { User } from '../models/user.model';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { JwtToken } from '../models/JwtToken.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  public jwtToken: BehaviorSubject<JwtToken> = new BehaviorSubject({
+    isAuthenticated: null,
+    token: null
+  });
+
   constructor(
     private http: HttpClient
-  ) { }
+  ) { this.initToken();}
 
   public signup(user: User): Observable<User> {
     return this.http.post<User>('/api/auth', user);
+  }
+
+  public signin(credentials: { email: string, password: string}): Observable<string> {
+    return this.http.post<string>('/api/auth/signin', credentials).pipe(
+      tap(( token: string ) => {
+        this.jwtToken.next({
+          isAuthenticated: true,
+          token: token
+        });
+        localStorage.setItem('jwt', token);
+      })
+    );
+  }
+
+  private initToken(): void {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      this.jwtToken.next({
+        isAuthenticated: true,
+        token: token
+      });
+    } else {
+      this.jwtToken.next({
+        isAuthenticated: false,
+        token: null
+      });
+    }
   }
 }
--- client/src/app/signin/signin.component.ts
+++ client/src/app/signin/signin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -8,9 +10,12 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class SigninComponent implements OnInit {
   public signinForm: FormGroup;
+  public error: string;
 
   constructor(
     private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -21,6 +26,10 @@ export class SigninComponent implements OnInit {
   }
 
   public trySignin() {
-    console.log(this.signinForm);
+    this.authService.signin(this.signinForm.value).subscribe( () => {
+      this.router.navigate(['/']);
+    }, err => {
+      this.error = err.error;
+    });
   }
 }
