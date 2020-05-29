a132e3b363d59c8f7eacf8e270b4ed65594886ee
sandrine <sandrinezocca@gmail.com>
2020-05-29 16:56:46 +0200

    token expiration

--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, timer, of, Subscription } from 'rxjs';
 import { User } from '../models/user.model';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+import { tap, switchMap } from 'rxjs/operators';
 import { JwtToken } from '../models/JwtToken.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +15,12 @@ export class AuthService {
     isAuthenticated: null,
     token: null
   });
+  subscription: Subscription;
 
-  constructor(
-    private http: HttpClient
-  ) { this.initToken();}
+  constructor(private http: HttpClient, private router: Router) { 
+    this.initToken();
+    this.subscription = this.initTimer();
+  }
 
   public signup(user: User): Observable<User> {
     return this.http.post<User>('/api/auth/signup', user);
@@ -31,9 +34,39 @@ export class AuthService {
           token: token
         });
         localStorage.setItem('jwt', token);
+        this.subscription = this.initTimer();
       })
     );
   }
+
+
+  public initTimer() {
+  return timer(2000, 5000).pipe(
+    switchMap(() => {
+      if (localStorage.getItem('jwt')) {
+        return this.http.get<string>('/api/auth/refresh-token').pipe(
+          tap((token: string) => {
+            this.jwtToken.next({
+              isAuthenticated: true,
+              token: token
+            });
+            localStorage.setItem('jwt', token);
+          })
+        );
+      } else {
+        this.subscription.unsubscribe();
+        return of(null);
+      }
+    })
+  ).subscribe(() => {}, err => {
+    this.jwtToken.next({
+      isAuthenticated: false,
+      token: null
+    });
+    localStorage.removeItem('jwt');
+    this.subscription.unsubscribe();
+  });
+}
 
   private initToken(): void {
     const token = localStorage.getItem('jwt');
@@ -57,6 +90,7 @@ export class AuthService {
         token: null
     });
     localStorage.removeItem('jwt');
+    this.router.navigate(['/signin']);
   }
   
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
 const RSA_KEY_PRIVATE = fs.readFileSync('./rsa/key');
+const RSA_PUBLIC_KEY = fs.readFileSync('./rsa/key.pub');
 
 router.post('/signin', (req, res) => {
   console.log(req.body);
@@ -12,6 +13,7 @@ router.post('/signin', (req, res) => {
     if (user && bcrypt.compareSync(req.body.password, user.password)) {
       const token = jwt.sign({}, RSA_KEY_PRIVATE, {
         algorithm: 'RS256',
+        expiresIn: '15s',
         subject: user._id.toString()
       })
       res.status(200).json(token);
@@ -20,6 +22,23 @@ router.post('/signin', (req, res) => {
     }
   })
 })
+
+router.get('/refresh-token', (req, res) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, RSA_PUBLIC_KEY, (err, decoded) => {
+      if (err) { return res.status(403).json('wrong token') }
+      const newToken = jwt.sign({}, RSA_KEY_PRIVATE, {
+        algorithm: 'RS256',
+        expiresIn: '15s',
+        subject: decoded.sub
+      })
+      res.status(200).json(newToken);
+    })
+  } else {
+    res.status(403).json('no token to refresh !');
+  }
+});
 
 router.post('/signup', (req,res) => {
   const newUser = new User({
